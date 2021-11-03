9f960418fac4164c4017a92b1c66b24f9541d7da
AmishMoses <77218460+AmishMoses@users.noreply.github.com>
2021-11-03 17:45:38 -0500

    Reworked exercise 10 Tuples

--- PY4E.CH10/EX_10.2.py
+++ PY4E.CH10/EX_10.2.py
@@ -19,3 +19,21 @@ each of the messages. You can pull the hour out from the 'From ' line by finding
 a second time using a colon.
 From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
 Once you have accumulated the counts for each hour, print out the counts, sorted by hour as shown below.'''
+
+'''Reworked exercise
+
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+times = {}
+handle = open(name)
+for line in handle:
+    line = line.strip() # Gives us clean lines of data to work with
+    if line.startswith('From '): # Gives us our list of senders
+        line = line.split() # Puts our data into a list format
+        hrminsec = line[5] # Gives us our time at index 5
+        hrminsecsplt = hrminsec.split(':') # Splits the data into a further list using ':' as the delim
+        hour = hrminsecsplt[0] # Grabs the hour section only
+        times[hour] = times.get(hour, 0) + 1 # Idiom to count the entries in the dict
+for key, value in sorted(times.items()):
+    print(key,value)'''
