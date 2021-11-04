d572a04000526ee04b06168a7753e9959fec2941
AmishMoses <77218460+AmishMoses@users.noreply.github.com>
2021-11-04 14:45:12 -0500

    Started the chapter on RegEx

---
+++ PY4e.CH11/Video_11.1.py
@@ -0,0 +1,38 @@
+#!/usr/env/ python
+import re #before you can use regex you have to import it into python
+'''Regular Expressions
+Regex provides a concise and flexible means for matching strings of text
+such as particular characters, words, or patters of characters.
+Regex uses wildcards to match zero, one, or more characters
+
+^ matches the beginning of a line
+$ matches the end of a line
+. matches any character
+\s matches whitespace
+\S matches non whitespace
+* repeats a character 0 or more times
+*? repeats a character 0 or more times (non-greedy)
++ repeats a character 1 or more times
++? repeats a character 1 or more times (non-greedy)
+[aeiou] matches a single character in the listed set
+[^xyz] matches a single character not in the listed set
+[a-z0-9] the set of characters can include a range
+( indicates where string extraction is to start 
+) indicates where string extraction is to end'''
+
+# You can use re.search() similar to find()
+# You can use the re.findall() to extract portions of a string that match your regular expression
+# Similar to the combination of find() and slicing: varname[:]
+'''Using re.search()
+for line in hand:
+    line = line.strip()
+    if re.search('From:', line): # Includes the variable line in the () instead of line.find('From: ')
+        print(line)
+        
+Using re.search() like .startswith()
+if re.search('^From:', line): # ^ means the beginning
+    print(line)     
+
+re.search('^x-\S+:', line) # ^Start with x- \S+ include and non-whitespace character one or more times end with :
+
+'''
---
+++ PY4e.CH11/Video_11.2.py
@@ -0,0 +1,39 @@
+#!/usr/env/ python
+import re
+# Extracting Data
+'''
+re.search() returns a T/F depending on the patterns match
+if we actually want the matching string to be extracted, we use re.findall()'''
+x = 'My 2 favorite numbers are 19 and 42'
+y = re.findall('[0-9]+', x)
+z = re.findall('^M.\s.', x) # Start with ^M . include any char \s include whitespace . end with any char
+novowel = re.findall('[^aeiou]', x) # Shows everything but vowels
+vowels = re.findall('[aeiou]\S+', x) # Come back to this
+print(vowels)
+
+# Greedy Matching
+# The repeat chars (* and +) push outward in both directions to match the largest string possible
+x = 'From: Using the : char'
+y = re.findall("^F.+:", x ) # This won't stop at the first : it will instead try to match the largest string possible
+print(y)
+z = re.findall('^F.+?:', x) # This is non-greedy and will end at the first : non-greedy = shortest
+print(z)
+
+# Fine Tuning string extraction
+# Emails
+x = 'Email from alexsemail@email.com'
+y = re.findall('\S+@\S+', x) # \S+ one or more non-whitespace chars followed by @ followed by \S+ again
+print(y)
+z = re.findall('^from (\S+@\S+)', x) # This says find a line that ^ w/ From and space then extract the \S+@\S+ data from x
+# () are not part of the match but they tell where to start and stop what string to extract
+# Finding just the email after the @ 
+a = re.findall('@(.*)', x) # start at @ extract (. any char * many occurences) from x
+# [^ ] means not a blank char
+y = re.findall('^From .*@([^ ]*)' , x)
+# Says line will ^From <space> .* include any number of chars before @ then begin extracting
+# ([^ ]*) says extract any non-blank char 0 or more times
+print(a)
+# If you want to match a wildcard just quote/escape the char with a \
+money = 'I have $432.21'
+m = re.findall('\$[0-9.]+', money) # find \$[0-9.]+ 0-9 including . repeating + one or more times
+print(m)
