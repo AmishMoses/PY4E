a75217b31404810ac628e261b3b6f73e7b3c9bc4
Alex <ttownusaa.af@gmail.com>
2021-11-09 15:02:18 -0600

    Finished working through Chapter 12 and the assignment

--- PY4E.CH12/WorkedExampleCode.py
+++ PY4E.CH12/WorkedExampleCode.py
@@ -3,7 +3,7 @@ import socket
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect(('data.pr4e.org', 80)) # This is a function call with a tuple inside
 # mysock.connect ( ( 'Hostname', port#))
-cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
 # .encode() converts from unicode (inside python) to UTF-8
 mysock.send(cmd)
 
