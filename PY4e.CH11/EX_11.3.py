02a09b7c4fd59572a94199c54a4d44226ff71f14
Alex <ttownusaa.af@gmail.com>
2021-11-09 02:01:42 -0600

    Finished EX 11.3 for Coursera, was still told my code could be better. Will reattempt to work out the logic tomorrow

--- PY4e.CH11/EX_11.3.py
+++ PY4e.CH11/EX_11.3.py
@@ -1,28 +1,14 @@
 #!/usr/bin/bash
 import re
-numlist = [] # Not sure whether to use a list or dict at the moment
-numdict = {} # So using both until i can figure it out
+print(sum(int(count) for count in re.findall('[0-9]+', open('C:/Users/bigal/Desktop/PY4E/PY4e.CH11/data.txt').read())))
+'''oneliner code print( sum( [ ****** *** * in **********('[0-9]+',**************************.read()) ] ) )'''
+numlist = []
 sum = 0
 name = input("Enter file:")
 if len(name) < 1:
     name = "C:/Users/bigal/Desktop/PY4E/PY4e.CH11/data.txt"
 handle = open(name)
-'''for line in handle:
-    line = line.strip() # Now we have our data read to lines
-    for nums in line: # Iterate through lines to find pattern
-        num = re.findall('[0-9]+', line) # Pattern should return 0-9+ any number 0-9 repeating
-        if len(num) != 1: continue
-        flt = int(num[0])
-        numlist.append(flt) # And append the numbers to a list so i can calculate sum
-print(sum(numlist))
-print(len(numlist))
-print(numlist)'''
-# But for some reason i'm getting a ton of [] empty list items
-# Tried using re.findall('[^\s][0-9]+.[0-9]+') to search for only non-whitespace but am still missing something
-# Tried ('[0-9]+.\S') # Started giving me .... 
 
-# Getting sum of 4687043 which is wrong 
-# Should end in 884 
 for line in handle:
     num = re.findall('[0-9]+', line) # Pattern should return 0-9+ any number 0-9 repeating
     numlist = numlist + num # Apend wouldn't work in the situation NoneType has no append attribute 
@@ -30,6 +16,9 @@ for line in handle:
 for data in numlist: # Data is our iteration variable
     sum = sum + int(data) # Parsing through the list and making each element an int before adding to var(sum)
 print(sum) # Correct 399884
+# My code still apparently isn't streamlined, but the TA loves to say it's wrong without and give riddles about how to fix it 
+# so it runs, it works, it stays
+
 
 
 
